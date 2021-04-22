c034de73074e913216c43c880312be0764e1a2d5
fabianusharry <fabianusharry@yahoo.com>
2021-04-22 10:49:52 +0700

    Table RKA render sudah founded

--- src/user/rka/RKA-Main.js
+++ src/user/rka/RKA-Main.js
@@ -51,18 +51,74 @@ export class RKAMain extends Component {
 
 	handleADOChange = (e) => {
 		console.log(this.currentADO.current.value);
+		console.log(this.state.RKA);
+	}
+
+	renderRKA = (rka, index) => {
+
+		var data = Object.values(rka.rancangan);
+		var id = data.splice(-1,1);
+		console.log(data);
+
+		return(
+			<tr key={index}>
+				<td>{rka.ADO}</td>
+				<td>{rka.kegiatan}</td>
+				<td>{rka.subkegiatan}</td>
+				<td>{rka.rincian_subkegiatan}</td>
+				<td>{rka.rincian_belanja}</td>
+				<td>{rka.jenis_belanja}</td>
+				{data.map(bulan => <td>{bulan}</td>)}
+			</tr>
+		)
 	}
 
 	render() {
 		return (
-				<form className="form-group" onSubmit={this.lihatRKA}>
-					<label htmlFor="select-ADO">Select ADO</label>
-					<select className="form-select form-select-sm" id="select-ADO" name="ado" onChange={this.handleADOChange} ref={this.currentADO}>
-						<option value = "ALL">All</option>
-						{ this.state.ADO.map(ADO => <option value={ADO}>{ADO}</option>) }
-					</select> <br />
-					<button className='btn btn-primary mt-2'>Lihat RKA</button>
-				</form>
+			<div class="container-fluid">
+				<div class="row">
+					<div className="col-sm">
+						<form className="form-group" onSubmit={this.lihatRKA}>
+							<label htmlFor="select-ADO">Select ADO</label>
+							<select className="form-select form-select-sm" id="select-ADO" name="ado"
+									onChange={this.handleADOChange} ref={this.currentADO}>
+								<option value="ALL">All</option>
+								{this.state.ADO.map(ADO => <option value={ADO}>{ADO}</option>)}
+							</select> <br/>
+							<button className='btn btn-primary mt-2'>Lihat RKA</button>
+						</form>
+					</div>
+
+					<div class="table">
+						<thead class="thead-dark">
+							<th>ADO</th>
+							<th>Kegiatan</th>
+							<th>Subkegiatan</th>
+							<th>Rincian Subkegiatan</th>
+							<th>Rincian Belanja</th>
+							<th>Jenis Belanja</th>
+							<th>Januari</th>
+							<th>Februari</th>
+							<th>Maret</th>
+							<th>April</th>
+							<th>Mei</th>
+							<th>Juni</th>
+							<th>Juli</th>
+							<th>Agustus</th>
+							<th>September</th>
+							<th>Oktober</th>
+							<th>November</th>
+							<th>Desember</th>
+						</thead>
+						<tbody class="table-striped">
+							{this.state.RKA.map(this.renderRKA)}
+						</tbody>
+					</div>
+				</div>
+
+			</div>
+
+
 		);
 	}
 }
