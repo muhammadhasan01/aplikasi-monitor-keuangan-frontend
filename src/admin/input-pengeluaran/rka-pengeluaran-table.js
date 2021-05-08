6e8e3b5eecb486b3779302036c583f4f3db4e675
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-08 14:50:42 +0700

    chore: add columns configuration

---
+++ src/admin/input-pengeluaran/rka-pengeluaran-table.js
@@ -0,0 +1,43 @@
+import { textFilter } from "react-bootstrap-table2-filter";
+import {formatRupiah} from "../../_helpers";
+
+export const tableData = {
+    getColumns
+};
+
+function getColumns(timeSlot) {
+    return [
+        {
+            dataField: "Rincian Belanja",
+            text: "Rincian Belanja"
+        },
+        {
+            dataField: "Alokasi Total",
+            text: "Alokasi Total",
+            sort: true,
+            formatter: formatRupiah,
+        },
+        {
+            dataField: "Alokasi Bulan",
+            text: `Alokasi Bulan ${timeSlot}`,
+            sort: true,
+            formatter: formatRupiah
+        },
+        {
+            dataField: "Penggunaan Bulan",
+            text: `Penggunaan Bulan ${timeSlot}`,
+            sort: true,
+            formatter: formatRupiah
+        },
+        {
+            dataField: "Sisa Anggaran Bulan",
+            text: `Sisa Anggaran Bulan ${timeSlot}`,
+            sort: true,
+            formatter: formatRupiah
+        },
+        {
+            dataField: "Aksi",
+            text: "Aksi"
+        }
+    ];
+}
