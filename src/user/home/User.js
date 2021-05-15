c5364a6f607afa8df3879362f021bbf16c077d07
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-15 18:08:16 +0700

    chore: change to mt-5 rather than mt-3

--- src/user/home/User.js
+++ src/user/home/User.js
@@ -6,7 +6,7 @@ export class User extends Component {
   render() {
     return (
       <Container className="d-flex row justify-content-center align-items-center">
-        <div className="mt-3 mr-5">
+        <div className="mt-5 mr-5">
           <HomeSVG scale={0.7} />
         </div>
         <h3>Selamat Datang di Aplikasi Monitor Keuangan!</h3>
