4b54d8e18557396395923e5346b6a05984c4d1b7
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-17 23:08:40 +0700

    feat: add riwayat pengeluaran

---
+++ src/admin/home/GrafikPengeluaran/TabelPengeluaran/TabelPengeluaran.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { RKADataService } from "_services";
+import { Loading } from "_components";
+import { dataPengeluaranTable } from "./data-table-pengeluaran";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory from "react-bootstrap-table2-filter";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import { Container } from "react-bootstrap";
+
+export class TabelPengeluaran extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      RKAs: null,
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveRKA();
+  }
+
+  retrieveRKA = () => {
+    RKADataService.getAllRKA()
+      .then(({ data }) => {
+        this.setState({ RKAs: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const { RKAs } = this.state;
+    if (!RKAs) {
+      return <Loading info={"tabel pengeluaran"} />;
+    }
+    const columns = dataPengeluaranTable.getColumns();
+    const data = dataPengeluaranTable.getData(RKAs);
+    console.log(columns);
+    console.log(data);
+    return (
+      <Container fluid style={{ width: "90%" }} className="mb-5">
+        <h3 className="mt-4">Tabel Pengeluaran </h3>
+        <BootstrapTable
+          classes="table-feature"
+          striped
+          bootstrap4
+          keyField="id"
+          responsive
+          wrapperClasses="table-responsive"
+          data={data}
+          columns={columns}
+          filter={filterFactory()}
+          pagination={paginationFactory()}
+          noDataIndication={"Belum ada data pengeluaran"}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/admin/riwayat-penggunaan/ModalAksiPengeluaran.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Modal, Button, Alert } from "react-bootstrap";
+
+export default class ModalAksiPengeluaran extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      showMessage: false,
+    };
+  }
+
+  static getDerivedStateFromProps(props) {
+    return { show: props.show, showMessage: props.showMessage };
+  }
+
+  handleCloseMessage = () => this.setState({ showMessage: false });
+
+  render() {
+    const { show, showMessage } = this.state;
+    const { action, handleConfirmation, handleClose } = this.props;
+    return (
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>{`Konfirmasi ${action} Pengeluaran`}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{`Apakah Anda yakin ingin melakukan ${action} pengeluaran?`}</p>
+          <div className="container">
+            <div className="row">
+              <Button
+                variant="success"
+                className="col text-center m-2"
+                onClick={handleConfirmation}
+                disabled={showMessage}
+              >
+                Ya
+              </Button>
+              <Button
+                variant="danger"
+                className="col text-center m-2"
+                onClick={handleClose}
+              >
+                Tidak
+              </Button>
+            </div>
+          </div>
+          <Alert className="m-2" show={showMessage} variant="success">
+            {`${action} Pengeluaran Berhasil!`}
+          </Alert>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
