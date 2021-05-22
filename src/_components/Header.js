65be3092bf11f797a5c620c0c719aa156cd30c46
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-05-23 03:05:58 +0700

    chore: change header

--- src/_components/Header.js
+++ src/_components/Header.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { Image } from "react-bootstrap";
+import { Col, Image, Row } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/all";
 
 export class Header extends Component {
   showUserInfo(userInfo) {
     const { name, type, unit, subunit } = userInfo;
     return (
-      <div style={{ textAlign: "right" }}>
+      <div>
         {name} <br />
         {type === "Admin" ? "Administrator" : unit + " - " + subunit}
       </div>
@@ -15,11 +16,21 @@ export class Header extends Component {
   render() {
     return (
       <div className="header">
-        <Image src="https://avatars.githubusercontent.com/u/8663791?s=280&v=4" />
-        <h1>Sistem Monitoring Anggaran</h1>
-        Sekolah Teknik Elektro dan Informatika <br />
-        Institut Teknologi Bandung <br />
-        {this.props.userInfo ? this.showUserInfo(this.props.userInfo) : null}
+        <Row>
+          <Col>
+            <Image src="https://avatars.githubusercontent.com/u/8663791?s=280&v=4" />
+            <h1>
+              <b>Sistem Monitoring Anggaran</b>
+            </h1>
+            Sekolah Teknik Elektro dan Informatika <br />
+            Institut Teknologi Bandung <br />
+          </Col>
+          <Col className="float-right text-right mt-3 mr-1">
+            {this.props.userInfo
+              ? this.showUserInfo(this.props.userInfo)
+              : null}
+          </Col>
+        </Row>
       </div>
     );
   }
